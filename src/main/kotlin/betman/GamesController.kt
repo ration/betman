71abfce056bb101ff61d1b381b9ceb7d72eb8a5d
Tatu Lahtela <lahtela@iki.fi>
2018-04-19 16:27:50 +0300

    move front to base

--- src/main/kotlin/betman/GamesController.kt
+++ src/main/kotlin/betman/GamesController.kt
@@ -3,9 +3,11 @@ package betman
 import betman.lsv.LsvAdapter
 import betman.pojos.Game
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/api")
 class GamesController {
 
     private val lsvAdapter = LsvAdapter()
