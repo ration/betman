c1a46044076beb949942672294f2d90afd9c4033
Tatu Lahtela <lahtela@iki.fi>
2018-04-21 23:58:17 +0300

    autosaving betting data

--- src/main/webapp/src/app/games.service.ts
+++ src/main/webapp/src/app/games.service.ts
@@ -1,15 +1,28 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpRequest} from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {Bet} from './bet.model';
 
 @Injectable()
 export class GamesService {
-  host = 'http://localhost:8080/';
-  apiAll = this.host + '/api/all';
+  private host = 'http://localhost:8080';
+  public apiAllUrl = this.host + '/api/all';
+  public saveBetUrl = this.host + '/api/addBets';
+  public betsUrl = this.host + '/api/bets';
 
   constructor(private http: HttpClient) {
   }
 
   all() {
-    return this.http.get(this.apiAll);
+    return this.http.get(this.apiAllUrl);
+  }
+
+  saveBet(game: string, user: string, bets: Bet[]) {
+    const params = new HttpParams().set('game', game).set('user', user);
+    return this.http.post(this.saveBetUrl, bets, {params});
+  }
+
+  bets(game: string, user: string) {
+    const params = new HttpParams().set('game', game).set('user', user);
+    return this.http.get(this.betsUrl, {params});
   }
 }
--- src/test/kotlin/betman/BettingControllerTest.kt
+++ src/test/kotlin/betman/BettingControllerTest.kt
@@ -1,13 +1,21 @@
 package betman
 
 import betman.pojos.Bet
+import org.junit.Assert
 import org.junit.Test
 
 class BettingControllerTest {
+    private val betting = BettingController()
+
     @Test
     fun sendScores() {
-        val betting = BettingController()
         val bets = listOf(Bet(1, 10,10))
-        betting.addBets(0, bets)
+        betting.addBets(0, 1, bets)
+    }
+
+    @Test
+    fun getScores() {
+        val ans = betting.bets(1,1)
+        Assert.assertEquals(48, ans.size)
     }
 }
