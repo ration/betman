88b2ce0a48b377a2f40d57f6453d232e7b77c21f
Tatu Lahtela <lahtela@iki.fi>
2018-04-21 23:58:17 +0300

    autosaving betting data

--- src/main/kotlin/betman/BettingController.kt
+++ src/main/kotlin/betman/BettingController.kt
@@ -1,21 +1,32 @@
 package betman
 
+import betman.lsv.LsvAdapter
 import betman.pojos.Bet
 import org.springframework.http.MediaType
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import java.util.logging.Logger
 
 @RestController
 @RequestMapping("/api")
 class BettingController {
     val LOG = Logger.getLogger(this.javaClass.name)
+    var userBets: List<Bet> = LsvAdapter().regularGames().map { Bet(it.id, 2, 3) }
 
-    @PostMapping("/addBets", consumes=[(MediaType.APPLICATION_JSON_VALUE)])
-    fun addBets(game:Int, @RequestBody bets: List<Bet>) {
-        LOG.info(String.format("received bets from %s", game))
+
+
+    @PostMapping("/addBets", consumes = [(MediaType.APPLICATION_JSON_VALUE)])
+    fun addBets(game: Int, user: Long, @RequestBody bets: List<Bet>) {
+        LOG.info(String.format("received bets from %s, %s", game, bets.toString()))
+        this.userBets = bets
+    }
+
+    @GetMapping("bets", produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun bets(game: Int, user: Long): List<Bet> {
+        // TODO actually from dao
+
+        return userBets
     }
 
 }
+
+
--- src/main/webapp/src/app/games.service.spec.ts
+++ src/main/webapp/src/app/games.service.spec.ts
@@ -1,7 +1,8 @@
 import {inject, TestBed} from '@angular/core/testing';
 
 import {GamesService} from './games.service';
-import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {Bet} from './bet.model';
 
 describe('GamesService', () => {
   beforeEach(() => {
@@ -20,18 +21,42 @@ describe('GamesService', () => {
     (httpMock: HttpTestingController, gamesService: GamesService) => {
 
       const mockGames = [{
-        "id": 1,
-        "home": {"name": "Russia"},
-        "away": {"name": "Saudi Arabia"},
-        "date": "2018-06-14T15:00:00.000+0000"
-      }, {"id": 2, "home": {"name": "Egypt"}, "away": {"name": "Uruguay"}, "date": "2018-06-15T12:00:00.000+0000"}];
+        'id': 1,
+        'home': {'name': 'Russia'},
+        'away': {'name': 'Saudi Arabia'},
+        'date': '2018-06-14T15:00:00.000+0000'
+      }, {'id': 2, 'home': {'name': 'Egypt'}, 'away': {'name': 'Uruguay'}, 'date': '2018-06-15T12:00:00.000+0000'}];
 
       gamesService.all().subscribe(res => {
         expect(res).toEqual(mockGames);
       });
-      const req = httpMock.expectOne("api/all");
+      const req = httpMock.expectOne(gamesService.apiAllUrl);
       expect(req.request.responseType).toEqual('json');
       req.flush(mockGames);
+      httpMock.verify();
+    }));
+
+  it('gets bets', inject(
+    [HttpTestingController, GamesService],
+    (httpMock: HttpTestingController, gamesService: GamesService) => {
+      const bets = [new Bet(1, 2, 3)];
+      gamesService.bets('1', '2').subscribe(res => {
+        expect(res).toBe(bets);
+      });
+      const req = httpMock.expectOne(gamesService.betsUrl + '?game=1&user=2');
+      req.flush(bets);
+      httpMock.verify();
+    }));
+
+  it('stores bets', inject(
+    [HttpTestingController, GamesService],
+    (httpMock: HttpTestingController, gamesService: GamesService) => {
+      const bets = [new Bet(1, 2, 3)];
+      gamesService.saveBet('1', '2', bets).subscribe(res => {
+        expect(res).toBe(null);
+      });
+      const req = httpMock.expectOne(gamesService.saveBetUrl + '?game=1&user=2');
+      req.flush(null);
       httpMock.verify();
     }));
 });
