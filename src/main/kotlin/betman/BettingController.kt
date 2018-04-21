7613b465178ab13721f9e12b017ce49aa0b22b41
Tatu Lahtela <tatu.lahtela@track4services.com>
2018-04-21 17:40:46 +0300

    some betting initial stuff

--- src/main/kotlin/betman/BettingController.kt
+++ src/main/kotlin/betman/BettingController.kt
@@ -1,5 +1,21 @@
 package betman
 
+import betman.pojos.Bet
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.util.logging.Logger
+
+@RestController
+@RequestMapping("/api")
 class BettingController {
+    val LOG = Logger.getLogger(this.javaClass.name)
+
+    @PostMapping("/addBets", consumes=[(MediaType.APPLICATION_JSON_VALUE)])
+    fun addBets(game:Int, @RequestBody bets: List<Bet>) {
+        LOG.info(String.format("received bets from %s", game))
+    }
 
 }
--- src/main/webapp/src/app/app.component.ts
+++ src/main/webapp/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {GamesService} from './games.service';
+import {Bet} from 'app/bet.model';
 
 @Component({
   selector: 'app-root',
@@ -9,12 +10,24 @@ import {GamesService} from './games.service';
 export class AppComponent {
   title = 'Betman';
   games = null;
+  bets = {};
 
 
   constructor(private gamesService: GamesService) {
     this.gamesService.all().subscribe(data => {
       this.games = data;
+      this.getBettingData();
     });
+  }
+
+  private getBettingData() {
+    for (const game of this.games) {
+      this.bets[game.id] = new Bet(game.id, 1, 2);
+    }
+  }
+
+  onChange(change) {
+    console.log(JSON.stringify(this.bets));
   }
 
   onSubmit() {
