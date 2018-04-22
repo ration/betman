f84f8d828d903a27abfc373f328a1968fed2e11a
Tatu Lahtela <lahtela@iki.fi>
2018-04-22 15:40:37 +0300

    login template, routing

--- src/main/webapp/src/app/app.module.ts
+++ src/main/webapp/src/app/app.module.ts
@@ -1,25 +1,43 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
 
 
-import { AppComponent } from './app.component';
-import { BettingComponent } from './betting/betting.component';
+import {AppComponent} from './app.component';
+import {BettingComponent} from './betting/betting.component';
 import {GamesService} from './games.service';
 import {FormsModule} from '@angular/forms';
+import {LoginComponent} from './login/login.component';
+import {RegisterComponent} from './register/register.component';
+
+import {AlertComponent} from './alert/alert.component';
+import {routing} from './app.routing';
+import {AlertService} from './alert.service';
+import {UserService} from './user.service';
+import {AuthGuard} from './auth.guard';
+import {HomeComponent} from './home/home.component';
+import {AuthenticationService} from './authentication.service';
+import {fakeBackendProvider} from './fakes/fake-backend';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    BettingComponent
+    BettingComponent,
+    LoginComponent,
+    RegisterComponent,
+    AlertComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    routing
   ],
-  providers: [GamesService],
+  providers: [GamesService, AlertService, UserService, AuthGuard, AuthenticationService,
+    fakeBackendProvider],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
