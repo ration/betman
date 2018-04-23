a76deaa3ac36f5f2498d457bf9aaacb2710aa89a
Tatu Lahtela <lahtela@iki.fi>
2018-04-24 00:02:16 +0300

    betting oddds

---
+++ src/main/webapp/src/app/fakes/odds.ts
@@ -0,0 +1,338 @@
+export const odds = [
+  {
+    'home': 'Russia',
+    'away': 'Saudi Arabia',
+    'homeOdds': '1.34',
+    'tieOdds': '4.82',
+    'awayOdds': '11.64'
+  },
+  {
+    'home': 'Egypt',
+    'away': 'Uruguay',
+    'homeOdds': '5.08',
+    'tieOdds': '3.78',
+    'awayOdds': '1.72'
+  },
+  {
+    'home': 'Morocco',
+    'away': 'Iran',
+    'homeOdds': '2.29',
+    'tieOdds': '3.08',
+    'awayOdds': '3.54'
+  },
+  {
+    'home': 'Portugal',
+    'away': 'Spain',
+    'homeOdds': '4.33',
+    'tieOdds': '3.48',
+    'awayOdds': '1.90'
+  },
+  {
+    'home': 'France',
+    'away': 'Australia',
+    'homeOdds': '1.21',
+    'tieOdds': '6.29',
+    'awayOdds': '17.83'
+  },
+  {
+    'home': 'Argentina',
+    'away': 'Iceland',
+    'homeOdds': '1.36',
+    'tieOdds': '4.60',
+    'awayOdds': '10.96'
+  },
+  {
+    'home': 'Peru',
+    'away': 'Denmark',
+    'homeOdds': '3.64',
+    'tieOdds': '3.39',
+    'awayOdds': '2.11'
+  },
+  {
+    'home': 'Croatia',
+    'away': 'Nigeria',
+    'homeOdds': '1.88',
+    'tieOdds': '3.46',
+    'awayOdds': '4.50'
+  },
+  {
+    'home': 'Costa Rica',
+    'away': 'Serbia',
+    'homeOdds': '4.06',
+    'tieOdds': '3.39',
+    'awayOdds': '2.00'
+  },
+  {
+    'home': 'Germany',
+    'away': 'Mexico',
+    'homeOdds': '1.48',
+    'tieOdds': '4.43',
+    'awayOdds': '7.26'
+  },
+  {
+    'home': 'Brazil',
+    'away': 'Switzerland',
+    'homeOdds': '1.37',
+    'tieOdds': '4.89',
+    'awayOdds': '9.24'
+  },
+  {
+    'home': 'Sweden',
+    'away': 'South Korea',
+    'homeOdds': '2.14',
+    'tieOdds': '3.28',
+    'awayOdds': '3.67'
+  },
+  {
+    'home': 'Belgium',
+    'away': 'Panama',
+    'homeOdds': '1.18',
+    'tieOdds': '7.17',
+    'awayOdds': '19.96'
+  },
+  {
+    'home': 'Tunisia',
+    'away': 'England',
+    'homeOdds': '9.45',
+    'tieOdds': '5.04',
+    'awayOdds': '1.35'
+  },
+  {
+    'home': 'Colombia',
+    'away': 'Japan',
+    'homeOdds': '1.80',
+    'tieOdds': '3.45',
+    'awayOdds': '5.07'
+  },
+  {
+    'home': 'Poland',
+    'away': 'Senegal',
+    'homeOdds': '2.28',
+    'tieOdds': '3.14',
+    'awayOdds': '3.49'
+  },
+  {
+    'home': 'Russia',
+    'away': 'Egypt',
+    'homeOdds': '1.88',
+    'tieOdds': '3.43',
+    'awayOdds': '4.51'
+  },
+  {
+    'home': 'Portugal',
+    'away': 'Morocco',
+    'homeOdds': '1.55',
+    'tieOdds': '3.91',
+    'awayOdds': '6.97'
+  },
+  {
+    'home': 'Uruguay',
+    'away': 'Saudi Arabia',
+    'homeOdds': '1.27',
+    'tieOdds': '5.55',
+    'awayOdds': '13.16'
+  },
+  {
+    'home': 'Iran',
+    'away': 'Spain',
+    'homeOdds': '18.13',
+    'tieOdds': '6.49',
+    'awayOdds': '1.20'
+  },
+  {
+    'home': 'Denmark',
+    'away': 'Australia',
+    'homeOdds': '1.79',
+    'tieOdds': '3.54',
+    'awayOdds': '4.90'
+  },
+  {
+    'home': 'France',
+    'away': 'Peru',
+    'homeOdds': '1.36',
+    'tieOdds': '4.89',
+    'awayOdds': '9.77'
+  },
+  {
+    'home': 'Argentina',
+    'away': 'Croatia',
+    'homeOdds': '1.83',
+    'tieOdds': '3.55',
+    'awayOdds': '4.65'
+  },
+  {
+    'home': 'Brazil',
+    'away': 'Costa Rica',
+    'homeOdds': '1.24',
+    'tieOdds': '6.22',
+    'awayOdds': '13.57'
+  },
+  {
+    'home': 'Nigeria',
+    'away': 'Iceland',
+    'homeOdds': '2.88',
+    'tieOdds': '3.17',
+    'awayOdds': '2.62'
+  },
+  {
+    'home': 'Serbia',
+    'away': 'Switzerland',
+    'homeOdds': '2.89',
+    'tieOdds': '3.17',
+    'awayOdds': '2.60'
+  },
+  {
+    'home': 'Belgium',
+    'away': 'Tunisia',
+    'homeOdds': '1.28',
+    'tieOdds': '5.60',
+    'awayOdds': '12.47'
+  },
+  {
+    'home': 'South Korea',
+    'away': 'Mexico',
+    'homeOdds': '4.08',
+    'tieOdds': '3.33',
+    'awayOdds': '2.00'
+  },
+  {
+    'home': 'Germany',
+    'away': 'Sweden',
+    'homeOdds': '1.54',
+    'tieOdds': '4.13',
+    'awayOdds': '6.68'
+  },
+  {
+    'home': 'England',
+    'away': 'Panama',
+    'homeOdds': '1.23',
+    'tieOdds': '6.04',
+    'awayOdds': '15.37'
+  },
+  {
+    'home': 'Japan',
+    'away': 'Senegal',
+    'homeOdds': '2.95',
+    'tieOdds': '3.18',
+    'awayOdds': '2.55'
+  },
+  {
+    'home': 'Poland',
+    'away': 'Colombia',
+    'homeOdds': '2.86',
+    'tieOdds': '3.19',
+    'awayOdds': '2.62'
+  },
+  {
+    'home': 'Saudi Arabia',
+    'away': 'Egypt',
+    'homeOdds': '5.15',
+    'tieOdds': '3.53',
+    'awayOdds': '1.75'
+  },
+  {
+    'home': 'Uruguay',
+    'away': 'Russia',
+    'homeOdds': '2.47',
+    'tieOdds': '3.19',
+    'awayOdds': '3.02'
+  },
+  {
+    'home': 'Iran',
+    'away': 'Portugal',
+    'homeOdds': '9.14',
+    'tieOdds': '4.68',
+    'awayOdds': '1.38'
+  },
+  {
+    'home': 'Spain',
+    'away': 'Morocco',
+    'homeOdds': '1.38',
+    'tieOdds': '4.73',
+    'awayOdds': '9.18'
+  },
+  {
+    'home': 'Australia',
+    'away': 'Peru',
+    'homeOdds': '3.33',
+    'tieOdds': '3.32',
+    'awayOdds': '2.23'
+  },
+  {
+    'home': 'Denmark',
+    'away': 'France',
+    'homeOdds': '5.64',
+    'tieOdds': '3.78',
+    'awayOdds': '1.65'
+  },
+  {
+    'home': 'Iceland',
+    'away': 'Croatia',
+    'homeOdds': '4.33',
+    'tieOdds': '3.51',
+    'awayOdds': '1.87'
+  },
+  {
+    'home': 'Nigeria',
+    'away': 'Argentina',
+    'homeOdds': '6.99',
+    'tieOdds': '4.16',
+    'awayOdds': '1.50'
+  },
+  {
+    'home': 'Mexico',
+    'away': 'Sweden',
+    'homeOdds': '2.47',
+    'tieOdds': '3.21',
+    'awayOdds': '3.00'
+  },
+  {
+    'home': 'South Korea',
+    'away': 'Germany',
+    'homeOdds': '11.64',
+    'tieOdds': '5.43',
+    'awayOdds': '1.28'
+  },
+  {
+    'home': 'Serbia',
+    'away': 'Brazil',
+    'homeOdds': '8.58',
+    'tieOdds': '4.72',
+    'awayOdds': '1.39'
+  },
+  {
+    'home': 'Switzerland',
+    'away': 'Costa Rica',
+    'homeOdds': '1.83',
+    'tieOdds': '3.48',
+    'awayOdds': '4.61'
+  },
+  {
+    'home': 'Japan',
+    'away': 'Poland',
+    'homeOdds': '4.22',
+    'tieOdds': '3.44',
+    'awayOdds': '1.91'
+  },
+  {
+    'home': 'Senegal',
+    'away': 'Colombia',
+    'homeOdds': '4.02',
+    'tieOdds': '3.40',
+    'awayOdds': '1.98'
+  },
+  {
+    'home': 'England',
+    'away': 'Belgium',
+    'homeOdds': '2.90',
+    'tieOdds': '3.26',
+    'awayOdds': '2.50'
+  },
+  {
+    'home': 'Panama',
+    'away': 'Tunisia',
+    'homeOdds': '3.40',
+    'tieOdds': '3.22',
+    'awayOdds': '2.25'
+  }
+];
