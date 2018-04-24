499f7d8f4d06778d3a04c385da4581b9ad867c11
Tatu Lahtela <lahtela@iki.fi>
2018-04-25 00:29:05 +0300

    hack hack

--- src/main/webapp/src/app/app.component.ts
+++ src/main/webapp/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import {Component} from '@angular/core';
+import {AfterViewChecked, AfterViewInit, Component, OnInit} from '@angular/core';
+import {AuthenticationService} from './authentication.service';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewChecked {
   title = 'Betman';
+
+  isLoggedIn: Observable<boolean>;
+
+  constructor(private authService: AuthenticationService) {
+
+  }
+
+  ngOnInit(): void {
+    this.isLoggedIn = this.authService.isLoggedIn();
+
+  }
+
+  ngAfterViewChecked() {
+  }
+
+
+  logout() {
+    this.authService.logout()
+  }
 }
