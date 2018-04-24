90ea295725f9387e5134b49dd4a7f559dc853584
Tatu Lahtela <lahtela@iki.fi>
2018-04-25 00:29:05 +0300

    hack hack

--- src/main/webapp/src/app/home/home.component.ts
+++ src/main/webapp/src/app/home/home.component.ts
@@ -17,18 +17,7 @@ export class HomeComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.loadAllUsers();
   }
 
-  deleteUser(id: number) {
-    this.userService.delete(id).subscribe(() => {
-      this.loadAllUsers();
-    });
-  }
 
-  private loadAllUsers() {
-    this.userService.getAll().subscribe(users => {
-      this.users = users;
-    });
-  }
 }
