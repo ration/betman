00824d9f073f7ee42aab2d7acc0d8ea117b2f36e
Tatu Lahtela <lahtela@iki.fi>
2018-04-26 19:30:19 +0300

    test commit, update author

--- src/main/kotlin/betman/GroupController.kt
+++ src/main/kotlin/betman/GroupController.kt
@@ -1,20 +1,24 @@
 package betman
 
+import betman.db.GroupRepository
+import betman.pojos.Group
 import betman.pojos.NewInvite
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.MediaType
 import org.springframework.security.oauth2.common.util.RandomValueStringGenerator
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api")
-class GroupController {
+class GroupController @Autowired constructor(private val repository: GroupRepository) {
 
-    @GetMapping("/group/new-invite")
-    fun newInvite(): NewInvite {
-        val generator = RandomValueStringGenerator()
-        generator.setLength(32)
-        return NewInvite(generator.generate())
+    @GetMapping("/group/new",  produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun newInvite(@RequestParam name: String,
+                  @RequestParam description: String): Group {
+        return repository.create(name,description)
     }
 
 }
