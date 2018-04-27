804021381dee730c360e6d418a127b83c6458aeb
Tatu Lahtela <lahtela@iki.fi>
2018-04-27 09:42:32 +0300

    fixes

--- src/main/kotlin/betman/BettingController.kt
+++ src/main/kotlin/betman/BettingController.kt
@@ -21,7 +21,7 @@ class BettingController {
     var userBets: List<Bet> = LsvAdapter().regularGames().map { Bet(it.id, 2, 3) }
 
 
-    @PostMapping("/addBets", consumes = [(MediaType.APPLICATION_JSON_VALUE)])
+    @PostMapping("/bets/update", consumes = [(MediaType.APPLICATION_JSON_VALUE)])
     fun addBets(game: Int, user: Long, @RequestBody bets: List<Bet>) {
         LOG.info(String.format("received bets from %s, %s", game, bets.toString()))
         this.userBets = bets
