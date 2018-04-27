d43e83714f57bcf17e67f4436ee5d5adae0b1689
Tatu Lahtela <lahtela@iki.fi>
2018-04-27 17:41:45 +0300

    tweak

--- src/main/kotlin/betman/DataLoader.kt
+++ src/main/kotlin/betman/DataLoader.kt
@@ -8,10 +8,9 @@ class DataLoader(private val provider: GameDataProvider,
                  private var gameRepository: GameRepository) {
 
     fun load() {
-        var game: Game? = gameRepository.get(provider.name)
-        if (game == null) {
-            game = gameRepository.create(Game(name = provider.name, description = provider.description))
-        }
+        val game: Game = gameRepository.get(provider.name)
+                ?: gameRepository.create(Game(name = provider.name, description = provider.description))
+
         val matches = provider.matches()
         val others = provider.others()
         gameRepository.update(game.withData(matches, others))
