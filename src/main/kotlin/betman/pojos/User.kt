b754eeda2558ad317e10ce410df8f744b462f019
Tatu Lahtela <lahtela@iki.fi>
2018-05-11 16:53:37 +0300

    misck

--- src/main/kotlin/betman/pojos/User.kt
+++ src/main/kotlin/betman/pojos/User.kt
@@ -1,4 +1,5 @@
 package betman.pojos
 
-data class User(val id: Int? = null, val name: String, val password: String? = null, val groups: List<Group> = listOf())
+data class User(val id: Int? = null, val name: String, val password: String? = null, val groups: List<Group> = listOf(),
+                val token: String? = null)
 
--- src/main/kotlin/betman/security/jwt/JwtAuthenticationFilter.kt
+++ src/main/kotlin/betman/security/jwt/JwtAuthenticationFilter.kt
@@ -1,8 +1,10 @@
 package betman.security.jwt
 
 import betman.pojos.User
+import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.KotlinModule
+import mu.KotlinLogging
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.Authentication
@@ -16,12 +18,14 @@ import javax.servlet.http.HttpServletResponse
 
 class JwtAuthenticationFilter(private val manager: AuthenticationManager,
                               private val tokenProvider: JwtTokenProvider) : UsernamePasswordAuthenticationFilter() {
+    private val jwtLogger = KotlinLogging.logger {} // <-- protected logger in base class :(
 
     private val mapper = ObjectMapper()
 
     init {
         authenticationManager = manager
         mapper.registerModule(KotlinModule())
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL)
     }
 
 
@@ -40,9 +44,14 @@ class JwtAuthenticationFilter(private val manager: AuthenticationManager,
                                           res: HttpServletResponse,
                                           chain: FilterChain,
                                           auth: Authentication) {
+
         val user = auth.principal as org.springframework.security.core.userdetails.User
+        jwtLogger.info("'{}' login successful", user.username)
         val token = tokenProvider.createToken(user.username, listOf("ROLE_USER"))
 
         res.addHeader("Authorization", "Bearer $token")
+        // Besides the header, let's put the token in the response as well
+        val response = User(name = user.username, token = token)
+        res.writer.print(mapper.writeValueAsString(response))
     }
 }
