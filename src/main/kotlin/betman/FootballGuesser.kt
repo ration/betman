facbd1a6f2d1eebe0f991dd77cf454fd77077d8a
Tatu Lahtela <lahtela@iki.fi>
2018-06-07 00:26:42 +0300

    Change gamma distributio to poisson estimates for probabilities

--- src/main/kotlin/betman/FootballGuesser.kt
+++ src/main/kotlin/betman/FootballGuesser.kt
@@ -1,19 +1,32 @@
 package betman
 
-import org.apache.commons.math3.distribution.GammaDistribution
+import org.apache.commons.math3.distribution.EnumeratedDistribution
+import org.apache.commons.math3.distribution.PoissonDistribution
+import java.lang.Math.exp
+import java.lang.Math.pow
 
 
 /**
  * Random score generator for Football scores
  */
 object FootballGuesser {
-
+    
     fun guess(odds: Double?): Int {
-        val base = odds ?: 1.0
-        val shape = 2.0 - (1 - 1 / base)
-        val scale = 1.2
-        val gamma = GammaDistribution(shape, scale)
-        return gamma.sample().toInt()
+        // We don't have the actual goal stats that poisson works best. Just do some guesstimation
+        val base = 1-(1-1/(odds ?: 1.0)) + 0.5
+        val distribution = (0..7).map { org.apache.commons.math3.util.Pair(it,poisson(it,base)) }
+        val engine = EnumeratedDistribution(distribution)
+        return engine.sample()
+    }
+
+    private fun factorial(n: Int): Double {
+        var fact = 1
+        for (i in 1..n) fact *= i
+        return fact.toDouble()
+    }
+
+    fun poisson(r: Int, mean: Double): Double {
+        return exp(-1 * mean) * pow(mean, r.toDouble()) / factorial(r)
     }
 
 }
--- src/test/kotlin/betman/FootballGuesserTest.kt
+++ src/test/kotlin/betman/FootballGuesserTest.kt
@@ -1,23 +1,50 @@
 package betman
 
-import org.junit.Assert.assertTrue
+import junit.framework.Assert.assertTrue
 import org.junit.Test
 
+enum class Winner {
+    HOME, AWAY, TIE
+}
+
+
 class FootballGuesserTest {
+
     @Test
     fun guess() {
-        val results = mutableListOf<Int>()
-        val results2 = mutableListOf<Int>()
+        val results = mutableListOf<Pair<Int, Int>>()
+        val results2 = mutableListOf<Pair<Int, Int>>()
 
-        for (i in 1..100) {
-            results += FootballGuesser.guess(1.2)
-            results2 += FootballGuesser.guess(10.2)
+        val results3 = mutableListOf<Pair<Int, Int>>()
+
+        for (i in 1..1000) {
+            results += Pair(FootballGuesser.guess(1.2), FootballGuesser.guess(10.2))
+            results2 += Pair(FootballGuesser.guess(1.5), FootballGuesser.guess(3.2))
+            results3 += Pair(FootballGuesser.guess(4.06), FootballGuesser.guess(2.00))
 
         }
-        println("results = ${results}")
-        println("results = ${results2}")
+        printStats(1.2, 10.2, results)
+        printStats(1.5, 3.2, results2)
+        printStats(4.06, 2.00, results3)
+        // This still has some probability for failure :(
+        assertTrue(results.filter{it.first >= it.second}.size > 900)
 
-        assertTrue(results.filter { it == 0 }.size < results2.filter { it == 0 }.size) // This can fail with some small probability if the
     }
 
+    private fun printStats(homeOdds: Double, awayOdds: Double, results: MutableList<Pair<Int, Int>>) {
+        //println("results = ${results}")
+        val ratio = results.groupBy {
+            val winner = it.first - it.second
+            when {
+                winner == 0 -> Winner.TIE
+                winner > 0 -> Winner.HOME
+                else -> Winner.AWAY
+            }
+        }
+        val home = (100 * ((ratio[Winner.HOME]?.size?.toDouble() ?: 0.0) / results.size.toDouble())).toInt()
+        val away = (100 * ((ratio[Winner.AWAY]?.size?.toDouble() ?: 0.0) / results.size.toDouble())).toInt()
+        val tie = (100 * ((ratio[Winner.TIE]?.size?.toDouble() ?: 0.0) / results.size.toDouble())).toInt()
+        println("Home $homeOdds Away $awayOdds $home/$tie/$away}")
+    }
 }
+
